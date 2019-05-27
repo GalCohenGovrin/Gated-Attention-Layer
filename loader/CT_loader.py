00a08cc6ef6f91a7b93fad53f195362fe5c75e32
GalCohenGovrin <galcohen@mail.tau.ac.il>
2019-05-27 11:58:03 +0300

    Update CT_loader.py

--- loader/CT_loader.py
+++ loader/CT_loader.py
@@ -15,27 +15,6 @@ from torchvision import transforms
 
 
 class CTLoader(data.Dataset):
-    """Data loader for the Pascal VOC semantic segmentation dataset.
-    Annotations from both the original VOC data (which consist of RGB images
-    in which colours map to specific classes) and the SBD (Berkely) dataset
-    (where annotations are stored as .mat files) are converted into a common
-    `label_mask` format.  Under this format, each mask is an (M,N) array of
-    integer values from 0 to 21, where 0 represents the background class.
-    The label masks are stored in a new folder, called `pre_encoded`, which
-    is added as a subdirectory of the `SegmentationClass` folder in the
-    original Pascal VOC data layout.
-    A total of five data splits are provided for working with the VOC data:
-        train: The original VOC 2012 training data - 1464 images
-        val: The original VOC 2012 validation data - 1449 images
-        trainval: The combination of `train` and `val` - 2913 images
-        train_aug: The unique images present in both the train split and
-                   training images from SBD: - 8829 images (the unique members
-                   of the result of combining lists of length 1464 and 8498)
-        train_aug_val: The original VOC 2012 validation data minus the images
-                   present in `train_aug` (This is done with the same logic as
-                   the validation set used in FCN PAMI paper, but with VOC 2012
-                   rather than VOC 2011) - 904 images
-    """
 
     def __init__(
         self,
@@ -81,8 +60,8 @@ class CTLoader(data.Dataset):
     #TODO: fix path to correct data
     def __getitem__(self, index):
         im_name = self.files[self.split][index]
-        im_path = pjoin(self.root, "train_val", "ct", self.split, "ct" + im_name)
-        lbl_path = pjoin(self.root, "train_val", "seg", self.split, "seg" + im_name)
+        im_path = pjoin(self.root, "fixed_data", "ct", self.split, "ct" + im_name)
+        lbl_path = pjoin(self.root, "fixed_data", "seg", self.split, "seg" + im_name)
         im = Image.open(im_path)
         lbl = Image.open(lbl_path)
         if self.augmentations is not None:
