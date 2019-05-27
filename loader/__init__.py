6dab890e6fe19d4a7f43e936ec6ca6be0a723c69
GalCohenGovrin <galcohen@mail.tau.ac.il>
2019-05-27 14:39:38 +0300

    Update __init__.py

--- loader/__init__.py
+++ loader/__init__.py
@@ -1,1 +1,11 @@
 from loader.CT_loader import CTLoader
+
+
+def get_loader(name):
+    """get_loader
+    :param name:
+    """
+    return {
+        "ct": CTLoader,
+        "CT": CTLoader
+    }[name]
