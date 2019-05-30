1d8b321ad4238126437b13b18a3b2da9930dddee
GalCohenGovrin <galcohen@mail.tau.ac.il>
2019-05-30 13:40:43 +0300

    Update CT_loader.py

--- loader/CT_loader.py
+++ loader/CT_loader.py
@@ -117,6 +117,28 @@ class CTLoader(data.Dataset):
                     fixed_img.save(pjoin(target_path, "ct", split, img_name), "PNG")
                     fixed_seg.save(pjoin(target_path, "seg", split, seg_name), "PNG")
                     
+                    
+def online_mean_and_sd(loader):
+    """Compute the mean and sd in an online fashion
+
+        Var[x] = E[X^2] - E^2[X]
+    """
+    cnt = 0
+    fst_moment = torch.empty(3)
+    snd_moment = torch.empty(3)
+
+    for data in loader:
+
+        b, c, h, w = data.shape
+        nb_pixels = b * h * w
+        sum_ = torch.sum(data, dim=[0, 2, 3])
+        sum_of_square = torch.sum(data ** 2, dim=[0, 2, 3])
+        fst_moment = (cnt * fst_moment + sum_) / (cnt + nb_pixels)
+        snd_moment = (cnt * snd_moment + sum_of_square) / (cnt + nb_pixels)
+
+        cnt += nb_pixels
+
+    return fst_moment, torch.sqrt(snd_moment - fst_moment ** 2)  
 
 
 # Leave code for debugging purposes
