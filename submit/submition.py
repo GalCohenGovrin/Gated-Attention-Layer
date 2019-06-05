46b0fc18dda14b2e1872470907334170dd040e30
GalCohenGovrin <galcohen@mail.tau.ac.il>
2019-06-05 16:09:39 +0300

    Update submition.py

--- submit/submition.py
+++ submit/submition.py
@@ -1,1 +1,33 @@
-"hey:"
+import torch
+import torch.nn.functional as F
+import numpy as np
+from tqdm import tqdm
+from torch.utils import data
+from PIL import Image
+import os
+from os.path import join as pjoin
+import matplotlib.pyplot as plt
+
+def save_predictions(preds, name, root = '/content/Data/'):
+    np_preds = preds.cpu().detach().numpy()
+    np_preds = np.squeeze(np_preds, axis=0)
+    
+    pred_mask = np.zeros_like(np_preds, dtype=int)
+    pred_mask[np_preds.max(axis=0,keepdims=1) == np_preds] = 1
+    
+    pred_bg = pred_mask[0, :, :]
+    pred_liver = pred_mask[1, :, :]
+    pred_lesion = pred_mask[2, :, :]
+    
+    final_mask = np.zeros_like(pred_bg, dtype=int)
+    
+    final_mask[pred_liver == 1] = 127
+    final_mask[pred_lesion == 1] = 255
+    
+    img_name = "seg" + name
+    path = pjoin(root, "fixed_data", "seg", "test",  im_name)
+    fixed_all_seg = Image.fromarray(final_mask)
+    fixed_all_seg.save(pjoin(path), "PNG")
+    
+  
+
